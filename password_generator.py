ddfba9829dd4deb3c8ce04286cbd80f8d736f5f6
ownesis <83978267+ownesis@users.noreply.github.com>
2022-01-25 18:12:05 +0100

    Clean this old ugly code *again* :eyes:

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import secrets
 import pyperclip
 
+MAX_NUM_RANGE = 110000
+
+password_manager = {
+    "Bitwarden": "https://bitwarden.com/",
+    "KeePass": "https://keepass.info/",
+    "NextCloud Password": "https://apps.nextcloud.com/apps/passwords"
+}
+
 secretsGenerator = secrets.SystemRandom()
 password = ""
-inc = 0
-long = input("Longueur du mot de passe : ")
+long = 0
 
-while inc != int(long):
-    random_number = secretsGenerator.randint(0, 110000)  # Generate a random number beetween 0 and 110 000
+# If the user does not insert a number
+while True:
+    user_input = input("Longueur du mot de passe : ")
+    try:
+        long = int(user_input)
+
+        if long <= 0:
+            print("Veuillez inserer un nombre positif superieur à 0.")
+            continue
+        
+        break
+    except ValueError:
+        print("Veuillez insérer un nombre valide.")
+
+for _ in range(long):
+    random_number = secretsGenerator.randint(0, MAX_NUM_RANGE)  # Generate a random number beetween 0 and 110 000
     if chr(random_number).isprintable():  # Check if the generated charactere is printable
         password += chr(random_number)
-        inc += 1
 
 pyperclip.copy(password)
-print("Votre mot de passe est :", password, "\nIl a été copié dans le presse papier \nN'oubliez pas de le stocker dans un gestionnaire de mot de passe tel que Bitwarden, Keepass ou Nextcloud Password")
+
+print("\nVotre mot de passe est : %s\n"
+      "Il a été copié dans le presse papier.\n" % password)
+
+print("N'oubliez pas de le stocker dans un gestionnaire de mot de passe tel que:")
+
+for k,v in password_manager.items():
+    print(" - %s: %s" % (k, v))
+
